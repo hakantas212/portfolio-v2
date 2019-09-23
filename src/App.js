a423e51dc99c9f4320ec69a345898c555470b8b2
Hakan <hakantas212@gmail.com>
2019-09-23 15:16:29 +0200

    Photos -section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import axios from "axios";
+import Photos from "./components/Photos";
 
 function App() {
+  const [images, setImages] = useState();
+  const accessKey =
+    "d5aeca2e0d9de8c9724b8ccffa6e1c7f0b9eefd73acc42ce1d64be3b421f8ad9";
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = () => {
+    axios
+      .get(
+        `https://api.unsplash.com/users/hakantas212/photos/?&per_page=50&client_id=${accessKey}`
+      )
+      .then(res => {
+        setImages(res.data);
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Photos images={images} />
     </div>
   );
 }
